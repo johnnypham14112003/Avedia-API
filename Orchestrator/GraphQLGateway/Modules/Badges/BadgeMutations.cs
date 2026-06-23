f80cd5938603458532ebe76cedad3d8521587358
Johnny Pham <johnnypham14112003@gmail.com>
2026-06-23 17:34:08 +0700

    add: badge graphql

---
+++ Orchestrator/GraphQLGateway/Modules/Badges/BadgeMutations.cs
@@ -0,0 +1,103 @@
+﻿using GraphQLGateway.DTOs.Request;
+using GraphQLGateway.Protos;
+using Mapster;
+
+namespace GraphQLGateway.Modules.Badges;
+
+[ExtendObjectType(OperationTypeNames.Mutation)]
+public class BadgeMutations
+{
+    public async Task<bool> CreateBadgeAsync(BadgeRq request,
+        BadgeGrpcService.BadgeGrpcServiceClient grpcClient)
+    {
+        // Create gRPC input
+        var badgeRequest = new BadgeRequest
+        {
+            BadgeInfo = request.Adapt<BadgeInfo>()
+        };
+
+        // Call gRPC
+        var response = await grpcClient.CreateBadgeAsync(badgeRequest);
+        if (!response.ResultResponse.Success)
+            throw new GraphQLException(response.ResultResponse.ErrorMessage);
+
+        return response.ResultResponse.Success;
+    }
+
+    public async Task<bool> AddBadgeToUserAsync(AccountBadgeRq request,
+        BadgeGrpcService.BadgeGrpcServiceClient grpcClient)
+    {
+        // Call gRPC
+        var response = await grpcClient.AddBadgeToUserAsync(request.Adapt<AccountBadgeRequest>());
+        if (!response.ResultResponse.Success)
+            throw new GraphQLException(response.ResultResponse.ErrorMessage);
+
+        return response.ResultResponse.Success;
+    }
+
+    public async Task<bool> UpdateBadgeAsync(BadgeRq request,
+        BadgeGrpcService.BadgeGrpcServiceClient grpcClient)
+    {
+        // Create gRPC input
+        var badgeRequest = new BadgeRequest
+        {
+            BadgeInfo = request.Adapt<BadgeInfo>()
+        };
+
+        // Call gRPC
+        var response = await grpcClient.UpdateBadgeAsync(badgeRequest);
+        if (!response.ResultResponse.Success)
+            throw new GraphQLException(response.ResultResponse.ErrorMessage);
+
+        return response.ResultResponse.Success;
+    }
+
+    public async Task<bool> DeleteBadgeAsync(Guid id,
+        BadgeGrpcService.BadgeGrpcServiceClient grpcClient)
+    {
+        // Create gRPC input
+        var input = new BadgeGetter
+        {
+            Id = id.ToString()
+        };
+
+        // Call gRPC
+        var response = await grpcClient.DeleteBadgeAsync(input);
+        if (!response.ResultResponse.Success)
+            throw new GraphQLException(response.ResultResponse.ErrorMessage);
+
+        return response.ResultResponse.Success;
+    }
+    public async Task<bool> DeletePermanentBadgeAsync(Guid id,
+        BadgeGrpcService.BadgeGrpcServiceClient grpcClient)
+    {
+        // Create gRPC input
+        var input = new BadgeGetter
+        {
+            Id = id.ToString()
+        };
+
+        // Call gRPC
+        var response = await grpcClient.DeletePermanentBadgeAsync(input);
+        if (!response.ResultResponse.Success)
+            throw new GraphQLException(response.ResultResponse.ErrorMessage);
+
+        return response.ResultResponse.Success;
+    }
+    public async Task<bool> RemoveAllBadgesFromAccountAsync(Guid id,
+        BadgeGrpcService.BadgeGrpcServiceClient grpcClient)
+    {
+        // Create gRPC input
+        var input = new BadgeGetter
+        {
+            Id = id.ToString()
+        };
+
+        // Call gRPC
+        var response = await grpcClient.RemoveAllBadgesFromAccountAsync(input);
+        if (!response.ResultResponse.Success)
+            throw new GraphQLException(response.ResultResponse.ErrorMessage);
+
+        return response.ResultResponse.Success;
+    }
+}
---
+++ Orchestrator/GraphQLGateway/Modules/Badges/BadgeQueries.cs
@@ -0,0 +1,64 @@
+﻿using GraphQLGateway.DTOs.Generic;
+using GraphQLGateway.DTOs.Request.Query;
+using GraphQLGateway.DTOs.Response;
+using GraphQLGateway.Protos;
+using Mapster;
+
+namespace GraphQLGateway.Modules.Badges;
+
+[ExtendObjectType(OperationTypeNames.Query)]
+public class BadgeQueries
+{
+    public async Task<BadgeRs> GetBadgeAsync(Guid id,
+        BadgeGrpcService.BadgeGrpcServiceClient grpcClient)
+    {
+        // Create gRPC input
+        var input = new BadgeGetter
+        {
+            Id = id.ToString()
+        };
+
+        // Call gRPC
+        var response = await grpcClient.GetBadgeAsync(input);
+        if (!response.ResultResponse.Success)
+            throw new GraphQLException(response.ResultResponse.ErrorMessage);
+
+        return response.BadgeInfo.Adapt<BadgeRs>();
+    }
+
+    public async Task<PagedResult<BadgeRs>> GetBadgesPageAsync(string? keyword, int pageNumber, int pageSize,
+        BadgeQr? badgeQr,
+        BadgeGrpcService.BadgeGrpcServiceClient grpcClient)
+    {
+        // Basic Validate
+        if (pageNumber < 1) pageNumber = 1;
+        if (pageSize < 1) pageSize = 10;
+
+        // Create gRPC input
+        var request = new BadgePageRequest
+        {
+            PageQueryRequest = new PageQueryRequest
+            {
+                Keyword = keyword ?? string.Empty,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            },
+            AdvanceInput = badgeQr.Adapt<BadgeQuery>()
+        };
+
+        // Call gRPC
+        var response = await grpcClient.GetBadgesPageAsync(request);
+        if (!response.ResultResponse.Success)
+            throw new GraphQLException(response.ResultResponse.ErrorMessage);
+
+        return new PagedResult<BadgeRs>
+        {
+            PageIndex = response.PagedData.BasePageResult.PageIndex,
+            PageSize = response.PagedData.BasePageResult.PageSize,
+            TotalCount = response.PagedData.BasePageResult.TotalCount,
+            TotalPage = response.PagedData.BasePageResult.TotalPage,
+            // Map gRPC message (AccountInfo) to GraphQL model (AccountRs)
+            DataList = [.. response.PagedData.DataList.Select(x => x.Adapt<BadgeRs>())]
+        };
+    }
+}
